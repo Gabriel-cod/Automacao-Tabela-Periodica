97212384701cc5f83b7372bc788b9faaa3aa63f3
Gabriel Alves Silva <ggalvesilva63@gmail.com>
2022-02-16 17:57:34 -0300

    versão 2.0

--- main.py
+++ main.py
@@ -1,35 +1,67 @@
 from os import system; from time import sleep; from defs import *
 saída = 'n'
 while saída not in 'Ss':
-    sleep(1.5)
     system('cls')
     print(f'Bem Vindo ao programa de Química')
     print('''- Informações sobre o programa:
-1º Vamos receber a quantidade de elétrons que o átomo, ou íon, possui;
+1º Vamos receber a quantidade de elétrons que o átomo, ou íon, possui ou o nome do elemento químico;
 2º Vamos retornar as informações possíveis sobre esse átomo/íon;
 3º O número quântico spin será considerado "-1/2" quando a seta estiver para cima (↑),
    e "1/2" quando a seta estiver para baixo (↓).\n''')
-    sleep(2)
-# Receber a informação de quantos elétrons o átomo possui
-    elé = nint('Informe a quantidade de elétrons do átomo/íon [somente números inteiros]: ')
-    ion = autenticar_sn('Esse átomo é um íon? ')
-    if ion in 'Ss':
-        elé = convert(elé)
-# Descobrir o elemento químico
-    elemento = elemqui(elé)
-# Descobrir a família
-    
-# Descobrir a camada de valência
+    # Receber a informação crucial sobre o átomo/íon
+    info = str(input('Dê alguma informação sobre o átomo/íon [nome ou quantidade de elétrons]: ')).strip().capitalize()
+    # primeira validação de informação
+    if info.isnumeric() or info.isalpha():
+        pass
+    else:
+        print('\nERRO: digite uma informação válida!')
+        sleep(2)
+        continue
+    # Processo através da quantidade de elétrons
+    if info.isnumeric():
+        elé = int(info)
+        # Segunda validação de informação
+        if elé > 118:
+            print('\nERRO: digite uma quantidade de elétrons válida!')
+            sleep(2)
+            continue
+        ion = autenticar_sn('Esse átomo é um íon? ')
+        if ion in 'Ss':
+            elé = convert(elé)
+        # Descobrir o elemento químico
+        elemento = elemqui(elé)
+    # Processo através do nome do elemento químico
+    elif info.isalpha():
+        erro = False
+        cont = 0
+        arq = open('elementos.txt', 'r', encoding='utf-8')
+        linhas = arq.readlines()
+        for l in linhas:
+            cont += 1
+            if info in l:
+                elemento = l
+                break
+            elif cont == 118:
+                erro = True
+                break
+        arq.close()
+        if erro:
+            print('ERRO: digite um elemento válido! [NÃO se esqueça de assentos, caso o elemento possua]')
+            sleep(3)
+            continue
+        elé = cont
+    # Descobrir a camada de valência
     sub, full = subnivel(elé)
-# Fazer a distribuição eletrônica
+    # Fazer a distribuição eletrônica
     deletronica = distribuição(full)
-# Criar lista de número quântico secundário
+    # Criar lista de número quântico secundário
     qsec = ['s', 'p', 'd', 'f']
-# Obter o número quântico magnético e spin
+    # Obter o número quântico magnético e spin
     ml, ms = magnético(sub)
-# Exibir as informações coletadas
+    # Exibir as informações coletadas
     print('\nRecebendo informações...\n')
     sleep(1)
+    print(f'Quantidade de elétrons: \t{elé}')
     print(f'Elemento químico: \t\t{elemento}')
     print(f'Subnível mais energético: \t{sub}')
     print(f'Número quântico principal (n): \t{sub[0]}')
@@ -40,5 +72,4 @@ while saída not in 'Ss':
     for c in deletronica:
         print(f'{c} ', end='')
     print(f'{sub}\n')
-
     saída = autenticar_sn('Deseja finalizar o programa? ')
